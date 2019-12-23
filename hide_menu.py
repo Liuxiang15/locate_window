347a0ec75b770e55a2e9ad793d1f0e2791049a21
Liuxiang15 <thss15_liux@163.com>
2019-12-23 17:26:12 +0800

    显示图标

--- hide_menu.py
+++ hide_menu.py
@@ -6,12 +6,14 @@
 import tkinter
 import win32api,win32con
 from tkinter import messagebox        #引入弹窗库
+from threading import Thread
 
 screen_x = win32api.GetSystemMetrics(win32con.SM_CXSCREEN)   #获得屏幕分辨率X轴
 screen_y = win32api.GetSystemMetrics(win32con.SM_CYSCREEN)   #获得屏幕分辨率Y轴
 cursor_width = 127
 root = tkinter.Tk()
 root.title("您可以在屏幕上点击您想要pygame窗口出现的位置!")
+root.resizable(width=False, height=False) # 设置窗口是否可以变化长/宽，False不可变，True可变，默认为True
 # root.overrideredirect(True)
 #root.attributes("-alpha", 0.3)窗口透明度70 %
 root.attributes("-alpha", 0.7)#窗口透明度60 %
@@ -40,13 +42,17 @@ canvas.pack()
 x, y = 0, 0
 
 def locate(event):
-    print(event.x)
-    print(event.y)
+    print(event.x, event.y)
     pos_gif = tkinter.PhotoImage(file=r"res\cursor.png")
+    marked_jpg = tkinter.PhotoImage(file=r"res\marked.png")
     offset = cursor_width / 2
-    canvas.create_image(event.x+offset, event.y+offset, image=pos_gif)
+    global canvas
+    canvas.create_image(event.x+offset, event.y+offset, image=pos_gif)  #这看上去是一个瞬间的操作，我们得加一个阻塞的事件
+    mark = canvas.create_image(event.x+offset, event.y+200, image=marked_jpg)  #这看上去是一个瞬间的操作，我们得加一个阻塞的事件
+    Thread(target=delete_mark, args=[mark]).start()
+    tkinter.mainloop()
     # show_click_pos(frame, event.x, event.y)
-    res = messagebox.askokcancel(title='定位确认',message='您是否确认定位在这里？')
+    # res = messagebox.askokcancel(title='定位确认',message='您是否确认定位在这里？')
     # if res == True:
     #     #Todo: 这里应该有一个在刚才点击位置
     #     pass
@@ -55,6 +61,13 @@ def locate(event):
     #         root.destroy()
     #         root = None
     # print(res)  
+
+def delete_mark(mark):
+    global canvas
+    import time
+    time.sleep(3)
+    canvas.delete(mark)
+    
 
 root.bind("<Button-1>",locate)
 def move(event):
